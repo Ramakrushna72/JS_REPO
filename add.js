255e6836c1ff0c67e89505cb197a7f6b8b05962f
Ramakrushna72 <ramakrushna7214@icloud.com>
2024-10-07 11:36:29 +0530

    code

---
+++ add.js
@@ -0,0 +1,11 @@
+var num1 = 78;
+var num2 = 45;
+let add = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+
+console.log("Addition of the given number is: ", add)
+console.log("substraction of the given number is: ", sub)
+console.log("multiplicatinnof the given number is: ", mul)
+console.log("Division of the given number is: ", div)
