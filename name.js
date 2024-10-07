86996471ff29ff84338228335d80a57920c438a8
Ramakrushna72 <ramakrushna7214@icloud.com>
2024-10-07 12:04:53 +0530

    code2

---
+++ name.js
@@ -0,0 +1,7 @@
+let name = "Ramakrushna"
+let city = "jaleswar"
+let age = 25
+let rollno = 2305275079
+let clg = "usbm"
+let batch = "2023-25"
+console.log(`my name is: ${name} ,city is: ${city}  ,age is: ${age} ,rollno is: ${rollno} , clg name is: ${clg},batch is: ${batch}`);
