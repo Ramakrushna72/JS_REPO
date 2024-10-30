260cbe089f4c928a4cd26bc42e8b6acd63c6a0be
Ramakrushna72 <ramakrushna7214@icloud.com>
2024-10-30 12:56:01 +0530

    new file added

---
+++ foreach.js
@@ -0,0 +1,28 @@
+//Higher Order function
+
+//forEach
+var arr = ['a', 'b', 'c', 'd'];
+
+// arr.forEach((e) => {
+//     console.log(e);
+// })
+
+
+//for in
+// for (let i in arr) {
+//     console.log(arr[i]);
+// }
+
+//for of loop
+
+// for (let i of arr) {
+//     console.log(i);
+// }
+
+//map function
+// arr.map((e) => {
+//     console.log(e + " Sunat")
+// })
+
+//reduce function
+
